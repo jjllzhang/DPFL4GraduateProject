c4979768eec679f37c8f3c6d305ad1d70932e906
JielongZhang <jielongzhang@sjtu.edu.cn>
2024-03-08 19:09:10 +0800

    chore: finish fed_avg experiments for FMNIST dataset
    Former-commit-id: 7b7b120d62c98d97a06ef3629027b2e587832032

--- models/FMNIST.py
+++ models/FMNIST.py
@@ -1,36 +1,32 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class FMNISTNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(2)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
-        self.drop = nn.Dropout2d(0.25)
-        self.fc2 = nn.Linear(in_features=600, out_features=120)
-        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(8, 2), stride=1, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 2), stride=1, padding=0)
+
+        # 使用一个辅助函数来计算卷积层输出的大小
+        fc_input_size = self._get_conv_output((1, 28, 28))
+
+        self.fc1 = nn.Linear(fc_input_size, 32)
+        self.fc2 = nn.Linear(32, 10)
+
+    def _get_conv_output(self, shape):
+        # 使用假数据通过卷积层来确定全连接层的输入大小
+        input = torch.autograd.Variable(torch.rand(1, *shape))
+        output = self.conv1(input)
+        output = self.conv2(output)
+        n_size = output.data.view(1, -1).size(1)
+        return n_size
         
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.drop(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)  # 展平操作
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        x = self.fc3(x)
         
         return x
