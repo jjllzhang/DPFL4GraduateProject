8429e59b9708e4136a8246a5b18df8c85e0bb978
JielongZhang <jielongzhang@sjtu.edu.cn>
2024-03-11 10:54:06 +0800

    feat: implement fed_avg_with_dp_with_shuffler algorithm
    Former-commit-id: 82fa5cd8a4e6e3120ecd02ad5b172233a59249eb

--- FL/utils/train_helper.py
+++ FL/utils/train_helper.py
@@ -1,6 +1,6 @@
 import yaml
 import torch
-
+import random
 
 def load_config(path):
     with open(path, 'r', encoding='utf-8') as file:
@@ -19,3 +19,10 @@ def load_model_and_optimizers(model, optimizers, load_path):
     model.load_state_dict(state['model_state'])
     for optimizer, state_dict in zip(optimizers, state['optimizers_state']):
         optimizer.load_state_dict(state_dict)
+
+
+def shuffle_weights(weight_of_each_client):
+    shuffled_weight_of_each_client = weight_of_each_client[:]
+    random.shuffle(shuffled_weight_of_each_client)
+    return shuffled_weight_of_each_client
+
