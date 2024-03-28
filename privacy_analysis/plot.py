777bf3bf11a7ad6314f712104a8d5ce93481edd2
zjl <jielongzhang@sjtu.edu.cn>
2024-03-28 12:55:01 +0800

    chore: test the influence of num_clients to privacy loss

--- privacy_analysis/plot.py
+++ privacy_analysis/plot.py
@@ -7,15 +7,14 @@ def plot_from_csv(csv_path, label):
     plt.plot(data['round'], data['epsilon'], label=label)
 
 # 曲线的名字
-labels = ['q=0.01', 'q=0.02', 'q=0.03','q=0.04', 'q=0.05', 'q=0.06']
+labels = ['N=5', 'N=10', 'N=20', 'N=50', 'N=100']
 
 # 读取文件并绘制曲线
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[0])
-plot_from_csv('./log/q_for_batch_size_0.02_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[1])
-plot_from_csv('./log/q_for_batch_size_0.03_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[2])
-plot_from_csv('./log/q_for_batch_size_0.04_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[3])
-plot_from_csv('./log/q_for_batch_size_0.05_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[4])
-plot_from_csv('./log/q_for_batch_size_0.06_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[5])
+plot_from_csv('./log/num_clients_5_q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[0])
+plot_from_csv('./log/num_clients_10_q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[1])
+plot_from_csv('./log/num_clients_20_q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[2])
+plot_from_csv('./log/num_clients_50_q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[3])
+plot_from_csv('./log/num_clients_100_q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[4])
 
 # 添加图例
 plt.legend()
@@ -31,4 +30,4 @@ plt.title('Privacy loss with δ = 1.1 x 1e-5')
 # 显示图形
 plt.show()
 
-plt.savefig('./log/privacy_loss_comparsion(sigma=1.0).png', dpi=300)
+plt.savefig('./log/privacy_loss_comparsion(q=0.01, sigma=1.0).png', dpi=300)
