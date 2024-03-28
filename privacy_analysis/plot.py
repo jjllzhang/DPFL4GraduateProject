f9fc140609d89e6795d650185279b530cef7e6e8
zjl <jielongzhang@sjtu.edu.cn>
2024-03-28 12:44:47 +0800

    chore: test the influence of q(sample rate) to privacy loss

--- privacy_analysis/plot.py
+++ privacy_analysis/plot.py
@@ -7,15 +7,15 @@ def plot_from_csv(csv_path, label):
     plt.plot(data['round'], data['epsilon'], label=label)
 
 # 曲线的名字
-labels = ['Without Shuffler, sigma=1.1', 'Without Shuffler, sigma=1.0', 'Without Shuffler, sigma=0.9','With Shuffler, sigma=1.1', 'With Shuffler, sigma=1.0', 'With Shuffler, sigma=0.9']
+labels = ['q=0.01', 'q=0.02', 'q=0.03','q=0.04', 'q=0.05', 'q=0.06']
 
 # 读取文件并绘制曲线
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.1_delta_1e-5_algorithm_fed_avg_with_dp.csv', labels[0])
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp.csv', labels[1])
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_0.9_delta_1e-5_algorithm_fed_avg_with_dp.csv', labels[2])
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.1_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[3])
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[4])
-plot_from_csv('./log/q_for_batch_size_0.01_sigma_0.9_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[5])
+plot_from_csv('./log/q_for_batch_size_0.01_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[0])
+plot_from_csv('./log/q_for_batch_size_0.02_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[1])
+plot_from_csv('./log/q_for_batch_size_0.03_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[2])
+plot_from_csv('./log/q_for_batch_size_0.04_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[3])
+plot_from_csv('./log/q_for_batch_size_0.05_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[4])
+plot_from_csv('./log/q_for_batch_size_0.06_sigma_1.0_delta_1e-5_algorithm_fed_avg_with_dp_with_shuffler.csv', labels[5])
 
 # 添加图例
 plt.legend()
@@ -26,9 +26,9 @@ plt.legend()
 # 设置坐标轴标签和标题
 plt.xlabel('T')
 plt.ylabel('ε')
-plt.title('Privacy loss with δ ≈ 1e-5')
+plt.title('Privacy loss with δ = 1.1 x 1e-5')
 
 # 显示图形
 plt.show()
 
-plt.savefig('./log/privacy_loss_comparsion.png', dpi=300)
+plt.savefig('./log/privacy_loss_comparsion(sigma=1.0).png', dpi=300)
